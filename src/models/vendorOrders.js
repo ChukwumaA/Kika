8ddc1209444d437bd644843e0415ce3ee7a5dabf
Ifechi <iamifechi@gmail.com>
2022-05-13 00:04:52 +0100

    order

--- src/models/vendorOrders.js
+++ src/models/vendorOrders.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const venderOrderSchema = new mongoose.Schema(
+const vendorOrderSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     quantity: { type: Number, required: true },
